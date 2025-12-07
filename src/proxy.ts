bec0929eb4478d82e8cf4d1a7b62cc9e50f219c5
Kyle D <kyle@trychroma.com>
2025-12-06 19:31:50 -0800

    middleware -> proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
     return NextResponse.next();
 
